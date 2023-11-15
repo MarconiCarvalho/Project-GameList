99d0b88b2472e2d6b2b6d70d31b79f0879570dee
Marconi de Carvalho Alves Junior <marconi.carvalho.bcc@gmail.com>
2023-11-15 20:30:40 -0300

    fist commit

---
+++ src/main/java/com/dev/projectGames/ProjectGamesApplication.java
@@ -0,0 +1,13 @@
+package com.dev.projectGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/projectGames/ProjectGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.projectGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
