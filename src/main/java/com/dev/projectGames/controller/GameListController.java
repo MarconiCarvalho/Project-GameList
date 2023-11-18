fed3012637f690478827b6783efaa16f493c7490
Marconi de Carvalho Alves Junior <marconi.carvalho.bcc@gmail.com>
2023-11-18 04:38:52 -0300

    GameList service, DTO

---
+++ src/main/java/com/dev/projectGames/controller/GameListController.java
@@ -0,0 +1,2 @@
+package com.dev.projectGames.controller;public class GameListController {
+}
---
+++ src/main/java/com/dev/projectGames/service/GameListService.java
@@ -0,0 +1,27 @@
+package com.dev.projectGames.service;
+
+import com.dev.projectGames.dto.GameDTO;
+import com.dev.projectGames.dto.GameMinDto;
+import com.dev.projectGames.entities.Game;
+import com.dev.projectGames.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository gameRepository;
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinDto> findAll(){
+      List<Game> result = gameRepository. findAll();
+      return  result.stream().map(x -> new GameMinDto(x)).toList();
+    }
+}
